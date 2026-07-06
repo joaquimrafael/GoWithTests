9866306307f7456955cb6327964e0ab757fe8e1b
joaquimprieto <joaquim.mpp@outlook.com>
2026-07-06 00:07:12 -0300

    Implement Server and SpyStore with tests for request handling and cancellation behavior

---
+++ context/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			log.Println("request canceled")
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
